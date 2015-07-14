8a7d9016f617756b4e494ff67410cb1cfe237098
GrandCircusPaul <paul@grandcircus.com>
2015-07-14 18:31:41 -0400

    Code for Fourth Demonstration Added
    The Code for the fourth presentation is added, including a new function
    in Program.cs that is called by the Main() function.

--- CodingNinjas_SAT/Program.cs
+++ CodingNinjas_SAT/Program.cs
@@ -13,10 +13,66 @@ namespace CodingNinjas_SAT
             //Run1(); //Travis, Cheryl, Matthew, Hema 
            
             //Run2(); //Nasir Derrick Kevin Vaneitta 
-            Run3(); //Adam Ashley Mara Uma
+            //Run3(); //Adam Ashley Mara Uma
+
+            //Run4(); //Shamseen, Greg, Nile, Mahmood
             Console.ReadLine();
+        }
 
+        static void Run4() //Student.cs
+        {
+            //creating a list of students
+            List<Student> team = new List<Student>();
 
+            //adding to team based on user input
+            makeTeam(team); 
+        }
+
+        //populate list based on user input
+        static void makeTeam(List<Student> team)
+        {
+            int size = 0, level = 0;
+            string name = "";
+
+            //asking how large user wants team to be
+            Console.Write("How many students do you want to add? >>");
+
+            //error-handling
+            while (!Int32.TryParse(Console.ReadLine(), out size) || size <= 0)
+                Console.WriteLine("Invalid input, please enter a positive integer for team size: ");
+
+            Console.Clear();
+
+            //setting input as the limiter for team
+            //asking for name and level of each student
+            for (int i = 0; i < size; i++)
+            {
+                //asking name
+                Console.Write("\nEnter student #{0}'s name: ", i + 1);
+                name = Console.ReadLine();
+
+                //asking level
+                Console.Write("Enter student #{0}'s level (integer number): ", i + 1);
+
+                //error-handling
+                while (!Int32.TryParse(Console.ReadLine(), out level) || level < 0)
+                    Console.Write("Invalid input, please enter a positive integer for student's level: ");
+
+                team.Add(new Student(name, level)); //instantiating new member
+            }
+
+            Console.Clear();
+            Console.WriteLine("This is your new team!");
+            foreach (Student s in team)
+                s.print();
+        }
+
+        //menu of options for user to modify students
+        static char menu(List<Student> team)
+        {
+            char input = 'Z';
+
+            return input;
         }
 
         static void Run3()//uses: "TheGame.cs"Adam Ashley Mara Uma
---
+++ CodingNinjas_SAT/Student.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingNinjas_SAT
+{
+    class Student
+    {
+        //Shamseen, Mahmood, Greg, Nile
+        public string name;
+        public int level;
+        string rank;
+
+        //default constructor (we never use it)
+        public Student()
+        {
+            level = 0;
+            rank = "";
+            name = "";
+        }
+
+        //overloaded constructor
+        public Student (string id, int pts = 0) //addition as per Cheryl
+        {
+            name = id;
+            level = pts;
+            findRank(); //addition as per Paul
+        }
+
+        //increment level by action based on user input
+        public void doSomething()
+        {
+            int action = 0;
+
+            //asking user how they would like to modify student
+            Console.Write("\nSelect a number: \nDid the student (1) Code a program,");
+            Console.Write(" (2) Assist another student, or (3) Do nothing?\n>>");
+            
+            //error checking. if TryParse doesn't work, action == 0.
+            action = 0;
+            Int32.TryParse(Console.ReadLine(), out action);
+
+            switch(action)
+            {
+                //student wrote a program
+                case 1:
+                    level++;
+                    Console.WriteLine("Student's level has increased by one!");
+                    break;
+                 
+                //student helped a peer
+                case 2:
+                    level += 2;
+                    Console.WriteLine("Student's level has increased by two!!");
+                    break;
+                
+                //user selected wrong student OR wanted to see everyone's stats.
+                case 3:
+                    return;
+
+                default: Console.WriteLine("Invalid Number.\n"); break;
+            }
+        }
+
+        //calculate rank based on level and printing a lil message with it
+        private string findRank()
+        {
+            string msg = "";
+
+            switch (level / 5)
+            {
+                case 0:
+                    rank = "Beginner";
+                    msg = "You are but a mere beginner still.";
+                    break;
+
+                case 1:
+                    rank = "Grasshopper";
+                    msg = "You are a fledgling grasshopper.";
+                    break;
+
+                case 2:
+                    rank = "Journeyman";
+                    msg = "You're a capable journeyman now!";
+                    break;
+
+                case 3:
+                    rank = "Rock Star";
+                    msg = "Looks like we have a programming rockstar on our hands!";
+                    break;
+
+                case 4:
+                    rank = "Ninja";
+                    msg = "What a programming ninja you are!";
+                    break;
+
+                case 5:
+                    rank = "Jedi";
+                    msg = "With you, the force is. You're a programming Jedi!";
+                    break;
+            }
+            return msg;
+        }
+        
+        //printing all info of student
+        public void print()
+        {
+            string msg = findRank();
+
+            Console.WriteLine("\n{0} is level {1} and rank {2}\n {3}", 
+                name, level, rank, msg);
+        }
+    }
+}
